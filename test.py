fec4bfafe649a1c32f841ad002f03f6f9ad40173
Andrey Litvin <57371484+kykazabra@users.noreply.github.com>
2026-09-15 17:15:21 +0300

    Remove generated artifacts and sample data from the repo

--- test.py
+++
@@ -1,7 +0,0 @@
-from main import DataAnalyst
-
-a = DataAnalyst(
-    r'C:\Users\CMD\PycharmProjects\agent_data_analyst\data (1).csv'
-)
-
-print(a.talk('Какое максимальное значение e5 в кждом федеральном регионе ?'))
